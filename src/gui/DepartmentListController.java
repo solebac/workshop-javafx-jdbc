2741079fc40930a638ed1d90ab69eb3f409e155d
solebac <solebac@hotmail.com>
2020-10-02 04:03:58 -0300

    Create Dialog Form

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,19 +1,28 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
 import entities.service.DepartmentService;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 
@@ -31,7 +40,9 @@ public class DepartmentListController implements Initializable {
 	@FXML
 	private Button btNew;
 	
-	public void onBtNewAction() {
+	public void onBtNewAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm(parentStage, "/gui/DepartmentForm.fxml");
 		System.out.println("onBtNewAction");
 	}
 	
@@ -63,6 +74,21 @@ public class DepartmentListController implements Initializable {
 		tableViewDepartment.setItems(objList);
 	}
 	
-	
+	private void createDialogForm(Stage parentStage, String path) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(path));
+			Pane pane = loader.load();
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter Department data");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);//Deixa o dialog na frente
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+			
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
 
 }
