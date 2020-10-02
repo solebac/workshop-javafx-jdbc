da19cd1d0aba79bc785474bfcc719746c0db6d38
solebac <solebac@hotmail.com>
2020-10-02 18:33:58 -0300

    Salvando um novo department

--- src/entities/service/DepartmentService.java
+++ src/entities/service/DepartmentService.java
@@ -1,15 +1,32 @@
 package entities.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
-	public static List<Department> findAll(){
+	
+	/*
+	 * Load via List
+	 * public static List<Department> findAll(){
 		List<Department> list = new ArrayList<>();
 		list.add(new Department(1, "Books"));
 		list.add(new Department(1, "Computers"));
 		return list;
+	}*/
+	private static DepartmentDao dao = DaoFactory.createDepartmentDao();
+	
+	public static List<Department> findAll(){
+		return dao.findAll();
 	}
+	public static void saveOrUpdate(Department obj) {
+		if (obj.getId() == null) {
+			dao.insert(obj);
+		}else {
+			dao.update(obj);
+		}
+	}
+	
 }
--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,9 +3,15 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DBException;
+import entities.service.DepartmentService;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -14,6 +20,11 @@ import model.entities.Department;
 public class DepartmentFormController implements Initializable {
 
 	private Department department;
+	private DepartmentService service;
+	
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
+	}
 	@FXML
 	private TextField txtId;
 	@FXML
@@ -26,11 +37,32 @@ public class DepartmentFormController implements Initializable {
 	private Button btCancel;
 	
 	@FXML
-	public void onBtSaveAction() {
-		System.out.println("onBtSaveAction");
+	public void onBtSaveAction(ActionEvent event) {
+		//Valida inject
+		if (department == null) {
+			throw new IllegalStateException("Entities was null");
+		}
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
+			department = getFormData();
+			service.saveOrUpdate(department);
+			Utils.currentStage(event).close();
+		} catch (DBException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+		//Test System.out.println("onBtSaveAction");
 	}
-	public void onBtCancelAction() {
-		System.out.println("onBtCancelAction");
+	private Department getFormData() {
+		Department obj = new Department();
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+		return obj;
+	}
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
+		//System.out.println("onBtCancelAction");
 	}
 	
 	@Override
