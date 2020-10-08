4565791ab03cab68e162b87c07085a0119727030
solebac <solebac@hotmail.com>
2020-10-08 02:20:07 -0300

    Textfield DatePicker

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,8 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
@@ -18,6 +21,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Seller;
@@ -37,7 +41,19 @@ public class SellerFormController implements Initializable {
 	@FXML
 	private TextField txtName;
 	@FXML
+	private TextField txtEmail;
+	@FXML
+	private DatePicker dpBirthDate;
+	@FXML
+	private TextField txtBaseSalary;
+	@FXML
 	private Label labelErrorName;
+	@FXML
+	private Label labelErrorEmail;
+	@FXML
+	private Label labelErrorBaseSalary;
+	@FXML
+	private Label labelErrorBirthDate;
 	@FXML
 	private Button btSave;
 	@FXML
@@ -103,7 +119,10 @@ public class SellerFormController implements Initializable {
 	private void initializeNodes() {
 		Constraints.setTextFieldInteger(txtId);
 		Constraints.setTextFieldMaxLength(txtName, 20);
-		
+		Constraints.setTextFieldMaxLength(txtEmail, 100);
+		Constraints.setTextFieldDouble(txtBaseSalary);
+		Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
+	
 	}
 	public void setSeller(Seller seller) {
 		this.seller = seller;
@@ -115,6 +134,12 @@ public class SellerFormController implements Initializable {
 		}
 		txtId.setText(String.valueOf(seller.getId()));
 		txtName.setText(seller.getName());
+		txtEmail.setText(seller.getEmail());
+		Locale.setDefault(Locale.US);
+		txtBaseSalary.setText(String.format("%.2f", seller.getBaseSalary()));
+		if (seller.getBirthDate() != null) {
+			dpBirthDate.setValue(LocalDate.ofInstant(seller.getBirthDate().toInstant(), ZoneId.systemDefault()));
+		}
 	}
 	public void subscribeDataChangeListener(DataChangeListener listener) {
 		dataChangeListeners.add(listener);
